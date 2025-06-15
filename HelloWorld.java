5f6b38031d59f730301013d118305b3fe3c0dc45
amitkmeher <amitkmeher@gmail.com>
2025-06-16 04:20:15 +0530

    Update HelloWorld.java
    new build

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ class Simple
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello, World welcome to my project batch-39");
-        System.out.println("The batch 39 started on 1st May 2025 and will end on 10th July 2025");
+        System.out.println("Hello, World welcome to my project for batch-39");
+        System.out.println("The batch 39 started on 1st May 2025 and it will end on 10th July 2025");
     }
 }
